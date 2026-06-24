b3aeb3d7450edc935144f729f2d85a83df80c9f5
Resilient Psyche <122923919+mikeisresilient@users.noreply.github.com>
2026-06-24 16:39:39 +0200

    Register Booking model in admin.py

--- workplace/littlelemon/restaurant/admin.py
+++ workplace/littlelemon/restaurant/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Menu
-from .models import Booking
+from .models import Menu, Booking
 
-
-# Register your models here.
 admin.site.register(Menu)
+admin.site.register(Booking)
